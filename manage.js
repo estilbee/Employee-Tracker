77c14a74e2acb3329e23fc49b0cacd5fe561a4e9
estilbee <odaouphars@gmail.com>
2023-01-10 15:11:42 -0500

    fixed bugs in menu options

--- manage.js
+++ manage.js
@@ -3,16 +3,16 @@ const connection = require('./connection')
 
 
 const mainMenu = () => {
-    inquirer.prompt([
-      {
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update employee role", "exit"],
-        name: "option"
-      }
-    ])
-    .then(({option}) => {
-      switch(option){
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "exit"],
+      name: "option"
+    }
+  ])
+    .then(({ option }) => {
+      switch (option) {
         case "view all departments":
           viewDepartments();
           break;
@@ -20,176 +20,181 @@ const mainMenu = () => {
           viewRoles();
           break;
         case "view all employees":
-            viewEmployees();
-            break;
+          viewEmployees();
+          break;
         case "add a department":
-            addDepartment();
-            break;
+          addDepartment();
+          break;
         case "add a role":
-            addRole();
-            break;
+          addRole();
+          break;
         case "add an employee":
-            addEmployee();
-            break;
+          addEmployee();
+          break;
         case "update an employee role":
-            updateEmployee();
-            break;
+          updateEmployee();
+          break;
         default:
-           program_exit();
+          program_exit();
       }
     });
-  };
-  
-  mainMenu();
+};
 
-  const addDepartment = () => {
-    inquirer.prompt([
-        {
-          type: "input",
-          message: "What is the department's name?",
-          name: "department_name"
-        }
-      ],
-      ) .then(data => {
-   connection.query(
-    `INSERT INTO department SET ?`, {name:data.department_name})
-    mainMenu(); })
+const addDepartment = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the department's name?",
+      name: "department_name"
     }
-
-  const addRole = () => {
-    connection.query("SELECT * FROM department", (err,res) => {
-    inquirer.prompt([
-        {
-          type: "input",
-          message: "What is the name of the role?",
-          name: "role_name"
-        },
-        {
-            type: "input",
-            message: "What is the salary of the role?",
-            name: "salary"
-        },
-        {
-            type: "list",
-            message: "What is the department of the role?",
-            choices: res.map(department => department.name),
-            name: "department"
-        }
-      ],) .then(data => { 
-        let selectedDepartment = res.find(department => department.name === data.department)
-        //find the department name that is = to the one in the res ans store it in a variable so we can get its id
-   connection.query(
-    `INSERT INTO role SET ?`, {title:data.role_name, salary:data.salary, department:data.department})  
+  ],
+  ).then(data => {
+    connection.query(
+      `INSERT INTO department SET ?`, { name: data.department_name })
     mainMenu();
-    
-})
-})  
-  }
+  })
+}
 
-  // const testAddEmployee = () => {
-  //   connection.query ('SELECT * FROM role', (err,res) => {
-  //       //inquirer prompt in here *follow the way addRole looks
-  //       //map role => role.title (instead of department.name) for role choices
-  //       //.then convert chosen role title to its id on INSERT INTO SET
-  //   })
-  // }
-
-
-  const addEmployee = () => {
-    connection.query ('SELECT * FROM role', (err,res) => {
-     inquirer.prompt([
-        {
-          type: "input",
-          message: "What is the first name of the employee?",
-          name: "first_name"
-        },
-        {
-            type: "input",
-            message: "What is the last name of the employee?",
-            name: "last_name"
-        },
-        {
-          type: "list",
-          message: "Which of the following is your employee's role?: ",
-          choices: res.map(role => role.title),
-          name: "role",
-        },
-        {
-            type: "input",
-            message: "Who is the employee's manager?",
-            name: "manager"
-        }
-      ])
-      .then(data => { 
-        // let selectedEmployee = res.find(department => department.name === data.department)
-      return connection.query(
-        `INSERT INTO employee SET ?`, 
-        {first_name:data.first_name, last_name:data.last_name, role:data.role, manager:data.manager})
-    
-        })
-    })
-  }
-
-  
-  const updateEmployee = () => {
-    connection.query ("SELECT * FROM employee", (err,res) => {
-      inquirer.prompt([
-        {
-          type: "input",
-          message: res.map(employee => employee.id),
-          name: "employee"
-          //should this be a list instead? the way the other functions are- and then you'd choose from the mapped employee list?
-      },
-      ])
-.then (data => {
-  const employeeID = data.employee
-   connection.query("SELECT * FROM role", (err,res) => {
+const addRole = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
     inquirer.prompt([
       {
         type: "input",
-        message: res.map(role => role.id),
-        name: "role"
-    },
-  ])
-  .then (data => {
-    const roleID = data.role
-    connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleID, employeeID]);
+        message: "What is the name of the role?",
+        name: "role_name"
+      },
+      {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "salary"
+      },
+      {
+        type: "list",
+        message: "What is the department of the role?",
+        choices: res.map(department => department.name),
+        name: "department"
+      }
+    ],).then(data => {
+      let selectedDepartment = res.find(department => department.name === data.department)
+      //find the department name that is = to the one in the res ans store it in a variable so we can get its id
+      connection.query(
+        `INSERT INTO role SET ?`, { title: data.role_name, salary: data.salary, department_id: selectedDepartment.id },
+        function (err) {
+          if (err) throw err
+          mainMenu();
+        }
+      )
+    })
+  })
+}
+
+
+const addEmployee = () => {
+  connection.query('SELECT * FROM role', (err, res) => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the first name of the employee?",
+        name: "first_name"
+      },
+      {
+        type: "input",
+        message: "What is the last name of the employee?",
+        name: "last_name"
+      },
+      {
+        type: "list",
+        message: "Which of the following is your employee's role?: ",
+        choices: res.map(role => role.title),
+        name: "role",
+      },
+      {
+        type: "list",
+        message: "What is the id of the employee's manager?",
+        choices: [1,2],
+        name: "manager"
+      }
+    ])
+      .then(data => {
+        let selectedEmployee = res.find(role => role.title === data.role)
+        connection.query(
+          `INSERT INTO employee SET ?`,
+          { first_name: data.first_name, last_name: data.last_name, role_id: selectedEmployee.id, manager_id: data.manager },
+          function (err) {
+            if (err) throw err
+              mainMenu(); }
+          )
+      })
+      
+  })
+}
+
+
+const updateEmployee = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose from the following employee ids:",
+        choices: res.map(employee => employee.id),
+        name: "employee"
+        //should this be a list instead? the way the other functions are- and then you'd choose from the mapped employee list?
+      },
+    ])
+      .then(data => {
+        const employeeID = data.employee
+        connection.query("SELECT * FROM role", (err, res) => {
+          inquirer.prompt([
+            {
+              type: "list",
+              message: "choose from the following role ids:",
+              choices: res.map(role => role.id),
+              name: "role"
+            },
+          ])
+            .then(data => {
+              const roleID = data.role
+              connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleID, employeeID]);
+              mainMenu();
+            })
+        })
+      })
+  }
+  )
+}
+
+
+const viewDepartments = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err
+    console.table(res)
     mainMenu();
   })
-})
-    })
-  }
-)}
+
+}
+const viewRoles = () => {
+  connection.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err
+    console.table(res)
+    mainMenu();
+  })
+
+}
+
+const viewEmployees = () => {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err
+    console.table(res)
+    mainMenu();
+  })
+
+}
 
 
-  const viewDepartments = () => {
-    connection.query ("SELECT * FROM department", (err,res) => {
-        if (err) throw err
-        console.table(res)
-        mainMenu();
-    } )
+const program_exit = () => {
+  // use this when you want to exit the script
+  connection.end();
+}
 
-  }
-  const viewRoles = () => {
-    connection.query ("SELECT * FROM role", (err,res) => {
-        if (err) throw err
-        console.table(res)
-        mainMenu();
-    } )
 
-  }
-
-  const viewEmployees = () => {
-    connection.query ("SELECT * FROM employee", (err,res) => {
-        if (err) throw err
-        console.table(res)
-        mainMenu();
-    } )
-
-  }
-
-  
-  const program_exit = () =>{
-    // use this when you want to exit the script
-    connection.end();
-  }
+mainMenu();
